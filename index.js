c4c98b71b2692eef082608d97d353b106d9c475d
NicoleLC16 <asalwaysnicole@outlook.com>
2021-04-12 13:25:20 -0700

    Done.

--- index.js
+++ index.js
@@ -1,12 +1,6 @@
-// const input = document.getElementById('input');
-// input.addEventListener('click', function(event) {
-//     alert('I was clicked!');
-// });
-
-
 function addingEventListener() {
-    const input = document.getElementById('input');
-    input.addEventListener('click', function(event) {
-        alert('I was clicked!');
-    });
+  const input = document.getElementById("input");
+  input.addEventListener("click", function (e) {
+    alert("I was clicked!");
+  });
 }
